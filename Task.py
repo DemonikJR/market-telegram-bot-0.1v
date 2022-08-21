f43cdbb4f7c8de6606e29b349088a74f00780818
DemonikJR <84394363+DemonikJR@users.noreply.github.com>
2022-08-21 22:35:42 +0300

    1.0v-release

--- Task.py
+++ Task.py
@@ -1,4 +1,14 @@
+import telebot
+
 class Task():
     isRunning = False
     isStaffOpen = False
+    isPay = False
+    yooacc_number = '410014635035950'
+    yootoken = ''
     token = ''
+    support_message = 'В разработке...'
+    bot_start_message = 'Привет ✌️! Я маркет-бот Name.\nДля взаимодействия со мной используй кнопочки ниже.\nПеред покупкой обязательно прочти правила!'
+    rules_message = 'В разработке...'
+    admins = ['DemonikJR', '']
+    bot = telebot.TeleBot(token)
